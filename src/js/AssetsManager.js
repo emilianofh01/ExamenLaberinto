744e3f3130d13449114a2e51582ca44881934688
Emiliano Fernández <emilianofh02@gmail.com>
2023-09-23 12:26:55 -0700

    Volumen ajustado(de nuevo)

--- src/js/AssetsManager.js
+++ src/js/AssetsManager.js
@@ -39,7 +39,7 @@ export class AssetManager {
       const audio = new Audio(a.source); 
       audio.preload = true;
       audio.autoplay = a.options.autoplay ?? false;
-      audio.volume = 0.5;
+      audio.volume = 0.25;
       audio.onloadedmetadata = this.checkLoadedAssets;
 
       audios[a.name] = audio;
